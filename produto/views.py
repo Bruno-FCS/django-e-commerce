8772c6167a9534645c25e67a0353ad4cc8db9e03
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2022-08-26 09:53:55 -0300

    Implementação da página de listagem dos produtos

--- produto/views.py
+++ produto/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
+from .models import Produto
 
 
 class ListaProdutos(ListView):
-    pass
+    model = Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 4
 
 
 class DetalheProduto(View):
